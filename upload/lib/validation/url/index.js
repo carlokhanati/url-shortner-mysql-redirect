e392ef00f37fd6ff01bdd5e4e429988d9296c5e3
Carlo A. Khanati <carlo.khanati@lebanonlab.bankaudilab.com>
2017-10-11 17:21:46 +0300

    split apps

--- upload/lib/validation/url/index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-exports.postUrl = require('./postUrl');
--- upload/lib/validation/url/postUrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Joi = require('joi');
-
-module.exports = {
-  body: {
-    url_code: Joi.string().alphanum().required()
-  }
-};
